6b5ca2e528cb211668b13a9fb65255dc8e7e06b3
marcelorocfer <marceloengecomp@gmail.com>
2024-07-29 11:09:34 -0300

    Create validations

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,43 @@
 class Customer {
-  _id: string;
-  _name: string;
-  _address: string;
 
-  constructor(id: string, name: string, address: string) {
+  _id: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = false;
+
+
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");      
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");      
+    }
+  }
+  
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
+  changeAddress(address: string): void {
     this._address = address;
   }
 
-  get id(): string {
-    return this._id;
+  activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");      
+    }
+    this._active = true;
   }
 
-  get name(): string {
-    return this._name;
-  }
-
-  get address(): string {
-    return this._address;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  set address(value: string) {
-    this._address = value;
+  deactivate() {
+    this._active = false;
   }
 }
