23a2e77611d593a60426bdfb2bb2748bb6f3fbaf
marcelorocfer <marceloengecomp@gmail.com>
2024-08-05 11:45:31 -0300

    Test reward points rules

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -47,9 +47,20 @@ describe("Customer unit tests", () => {
       const customer = new Customer("1", "Customer 1");
 
       customer.deactivate();
-      
+
       expect(customer.isActive()).toBe(false);
     });
+  });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
   });
 
 });
