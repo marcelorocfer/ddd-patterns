3b2e31d4f2cab24b6b691e95e0ebfb50164723e2
marcelorocfer <marceloengecomp@gmail.com>
2024-08-06 11:02:00 -0300

    Set sequelize

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize-typescript";
+
+describe("Product repository test", () => {
+
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    
+    afterEach(async () => {
+      await sequelize.close();
+    });
+  });
+
+
+
+  it("should create a new product", async () => {});
+
+});
