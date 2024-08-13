76797a407c9bd08e1fda30220182eccc7b59beca
marcelorocfer <marceloengecomp@gmail.com>
2024-08-13 10:33:44 -0300

    Register the first event

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,4 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import { EventDispatcher } from "./event-dispatcher";
 
 describe("Domain events tests", () => {
@@ -7,7 +8,7 @@ describe("Domain events tests", () => {
 
     eventDispatcher.register("ProductCreatedEvent", eventHandler);
 
-    expect(eventDispatcher.getHandlers("ProductCreatedEvent")).toBeDefined();
-    expect(eventDispatcher.getHandlers("ProductCreatedEvent").length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
   });
 });
--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,20 +3,23 @@ import EventHandlerInterface from "./event-handler.interface";
 import EventInterface from "./event.interface";
 
 export class EventDispatcher implements EventDispatcherInterface {
-  notify(event: EventInterface): void {
-    throw new Error("Method not implemented.");
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
   }
   
   register(eventName: string, eventHandler: EventHandlerInterface): void {
-    throw new Error("Method not implemented.");
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
   }
   
-  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
-    throw new Error("Method not implemented.");
-  }
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {}
   
-  unregisterAll(): void {
-    throw new Error("Method not implemented.");
-  }
-  
+  unregisterAll(): void {}
+
+  notify(event: EventInterface): void {}
+
 }
