0506e65de38bdc789472b7a9b0d810c9fa18feb3
marcelorocfer <marceloengecomp@gmail.com>
2024-08-15 11:53:59 -0300

    Modularize domain layer

--- src/domain/checkout/service/order.service.spec.ts
+++ src/domain/checkout/service/order.service.spec.ts
@@ -1,7 +1,7 @@
-import Customer from "../entity/customer";
 import Order from "../entity/order";
-import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
+import OrderItem from "../entity/order_item";
+import Customer from "../../entity/customer";
 
 describe("Order service unit tests", () => {
 
--- src/domain/checkout/service/order.service.ts
+++ src/domain/checkout/service/order.service.ts
@@ -1,7 +1,7 @@
-import Customer from "../entity/customer";
-import Order from "../entity/order";
-import OrderItem from "../entity/order_item";
 import { v4 as uuid } from "uuid";
+import Order from "../entity/order";
+import Customer from "../../entity/customer";
+import OrderItem from "../entity/order_item";
 
 export default class OrderService {  
   static placeOrder(customer: Customer, items: OrderItem[]): Order {
